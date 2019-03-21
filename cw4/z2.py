59e552be9e4b408afc42a4bbc0bb516458561f49
danskiiiii <danielwil@wp.pl>
2019-03-21 15:58:03 +0100

    c4

--- cw4/z2.py
+++ cw4/z2.py
@@ -1,3 +1,13 @@
+from scipy.special import comb as binom # example: binom(10, 5, exact=True) => 252
 n = int(input('Enter n: '))
 t = input('Enter T: ')
 subset = t.split()
+subset = [ int(num) for num in subset ]
+k= len(subset)
+
+rank=0
+subset.insert(0,0)
+for i in range(1,k+1):
+    for j in range(subset[i-1]+1, subset[i]):
+        rank += binom(n-j, k-i, exact=True)
+print(rank)
